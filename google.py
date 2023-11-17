4069e2d55bf3e956ff85b87a50d4cb57c0a00dc5
cushico <luisjaramillo90@gmail.com>
2023-11-17 10:21:41 -0500

    aas

--- google.py
+++
@@ -1,7 +0,0 @@
-import base64
-# Cadena codificada en base64
-cadena_base64 = "sBAAAAcWdckibwA0qHDnej40ObBAAAAAACAAAAAAAQZgAAAAEAACAAAAAlIq/NHEijc63XldtP"
-# Decodificar la cadena
-cadena_decodificada = base64.b64decode(cadena_base64)
-texto_decodificado = cadena_decodificada.decode('utf-8')
-print("Cadena decodificada:", texto_decodificado)
