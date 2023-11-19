16b141bd911c6df1a70a99a1bd7def41d9d7e102
cushico <luisjaramillo90@gmail.com>
2023-11-18 22:50:42 -0500

    carpeta para ejercicios

---
+++ códigos/java/Persona.java
@@ -0,0 +1,16 @@
+public class Persona {
+    int edad;
+    String nombre;
+
+    public Persona(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+    
+    public void Saltar(){
+        System.out.println("El usuario salta");
+    }
+    
+    
+    
+}
---
+++ códigos/java/main.java
@@ -0,0 +1,8 @@
+public class POO {
+
+    public static void main(String[] args) {
+        System.out.println("H World!");
+        Persona p1 = new Persona(33,"luis");
+        //p1.Saltar();
+    }
+}
