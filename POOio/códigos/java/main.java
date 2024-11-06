16921a024be9972b76eedce9f5bcf4118d5a32fa
cushico <luisjaramillo90@gmail.com>
2024-11-06 08:12:54 -0500

    poo v1
    version1

--- POOio/códigos/java/main.java
+++
@@ -1,8 +0,0 @@
-public class POO {
-
-    public static void main(String[] args) {
-        System.out.println("H World!");
-        Persona p1 = new Persona(33,"luis");
-        //p1.Saltar();
-    }
-}
